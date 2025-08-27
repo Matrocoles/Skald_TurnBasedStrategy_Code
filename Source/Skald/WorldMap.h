9884d3fa1e9c4f85b8e451711c11a6e5a2255a81
Matrocoles <83141363+Matrocoles@users.noreply.github.com>
2025-08-27 01:37:42 -0400

    Use TObjectPtr for world map territories

--- Source/Skald/WorldMap.h
+++ Source/Skald/WorldMap.h
@@ -13,105 +13,93 @@ DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FWorldMapTerritorySelected,
 
 /** Data describing how a territory should be spawned at runtime. */
 USTRUCT(BlueprintType)
-struct FTerritorySpawnData
-{
-    GENERATED_BODY();
+struct FTerritorySpawnData {
+  GENERATED_BODY();
 
-    FTerritorySpawnData()
-        : TerritoryID(0)
-        , TerritoryName(TEXT(""))
-        , bIsCapital(false)
-        , ContinentID(0)
-        , Location(FVector::ZeroVector)
-    {
-    }
+  FTerritorySpawnData()
+      : TerritoryID(0), TerritoryName(TEXT("")), bIsCapital(false),
+        ContinentID(0), Location(FVector::ZeroVector) {}
 
-    FTerritorySpawnData(int32 InID, const FString& InName, bool bCapital = false,
-                        int32 InContinent = 0, FVector InLocation = FVector::ZeroVector)
-        : TerritoryID(InID)
-        , TerritoryName(InName)
-        , bIsCapital(bCapital)
-        , ContinentID(InContinent)
-        , Location(InLocation)
-    {
-    }
+  FTerritorySpawnData(int32 InID, const FString &InName, bool bCapital = false,
+                      int32 InContinent = 0,
+                      FVector InLocation = FVector::ZeroVector)
+      : TerritoryID(InID), TerritoryName(InName), bIsCapital(bCapital),
+        ContinentID(InContinent), Location(InLocation) {}
 
-    UPROPERTY(EditAnywhere, BlueprintReadOnly)
-    int32 TerritoryID;
+  UPROPERTY(EditAnywhere, BlueprintReadOnly)
+  int32 TerritoryID;
 
-    UPROPERTY(EditAnywhere, BlueprintReadOnly)
-    FString TerritoryName;
+  UPROPERTY(EditAnywhere, BlueprintReadOnly)
+  FString TerritoryName;
 
-    UPROPERTY(EditAnywhere, BlueprintReadOnly)
-    bool bIsCapital;
+  UPROPERTY(EditAnywhere, BlueprintReadOnly)
+  bool bIsCapital;
 
-    UPROPERTY(EditAnywhere, BlueprintReadOnly)
-    int32 ContinentID;
+  UPROPERTY(EditAnywhere, BlueprintReadOnly)
+  int32 ContinentID;
 
-    /** Location for spawning this territory relative to the world map actor. */
-    UPROPERTY(EditAnywhere, BlueprintReadOnly)
-    FVector Location;
+  /** Location for spawning this territory relative to the world map actor. */
+  UPROPERTY(EditAnywhere, BlueprintReadOnly)
+  FVector Location;
 };
 
 /**
  * Actor owning and managing all territories in the map.
  */
 UCLASS(Blueprintable, BlueprintType)
-class SKALD_API AWorldMap : public AActor
-{
-    GENERATED_BODY()
+class SKALD_API AWorldMap : public AActor {
+  GENERATED_BODY()
 
 public:
-    AWorldMap();
+  AWorldMap();
 
-    virtual void BeginPlay() override;
+  virtual void BeginPlay() override;
 
-    /** All territories contained in this world map. */
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "WorldMap")
-    TArray<ATerritory*> Territories;
+  /** All territories contained in this world map. */
+  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "WorldMap")
+  TArray<TObjectPtr<ATerritory>> Territories;
 
-    /** Currently selected territory. */
-    UPROPERTY(BlueprintReadOnly, Category = "WorldMap")
-    ATerritory* SelectedTerritory;
+  /** Currently selected territory. */
+  UPROPERTY(BlueprintReadOnly, Category = "WorldMap")
+  ATerritory *SelectedTerritory;
 
-    /** Event fired whenever SelectTerritory chooses a new territory. */
-    UPROPERTY(BlueprintAssignable, Category = "WorldMap")
-    FWorldMapTerritorySelected OnTerritorySelected;
+  /** Event fired whenever SelectTerritory chooses a new territory. */
+  UPROPERTY(BlueprintAssignable, Category = "WorldMap")
+  FWorldMapTerritorySelected OnTerritorySelected;
 
-    /** Register a territory with the world map. */
-    UFUNCTION(BlueprintCallable, Category = "WorldMap")
-    void RegisterTerritory(ATerritory* Territory);
+  /** Register a territory with the world map. */
+  UFUNCTION(BlueprintCallable, Category = "WorldMap")
+  void RegisterTerritory(ATerritory *Territory);
 
-    /** Get a territory by its identifier. */
-    UFUNCTION(BlueprintCallable, Category = "WorldMap")
-    ATerritory* GetTerritoryById(int32 TerritoryId) const;
+  /** Get a territory by its identifier. */
+  UFUNCTION(BlueprintCallable, Category = "WorldMap")
+  ATerritory *GetTerritoryById(int32 TerritoryId) const;
 
-    /** Handle territory selection. */
-    UFUNCTION(BlueprintCallable, Category = "WorldMap")
-    void SelectTerritory(ATerritory* Territory);
+  /** Handle territory selection. */
+  UFUNCTION(BlueprintCallable, Category = "WorldMap")
+  void SelectTerritory(ATerritory *Territory);
 
-    /** Move units between territories. */
-    UFUNCTION(BlueprintCallable, Category = "WorldMap")
-    bool MoveBetween(ATerritory* From, ATerritory* To);
+  /** Move units between territories. */
+  UFUNCTION(BlueprintCallable, Category = "WorldMap")
+  bool MoveBetween(ATerritory *From, ATerritory *To);
 
-    /** Actor class used when spawning territory instances. */
-    UPROPERTY(EditAnywhere, Category = "WorldMap")
-    TSubclassOf<ATerritory> TerritoryClass;
+  /** Actor class used when spawning territory instances. */
+  UPROPERTY(EditAnywhere, Category = "WorldMap")
+  TSubclassOf<ATerritory> TerritoryClass;
 
-    /** Minimum XY (in local space) for random spawn positions. */
-    UPROPERTY(EditAnywhere, Category = "WorldMap")
-    FVector2D SpawnAreaMin = FVector2D(-500.f, -500.f);
+  /** Minimum XY (in local space) for random spawn positions. */
+  UPROPERTY(EditAnywhere, Category = "WorldMap")
+  FVector2D SpawnAreaMin = FVector2D(-500.f, -500.f);
 
-    /** Maximum XY (in local space) for random spawn positions. */
-    UPROPERTY(EditAnywhere, Category = "WorldMap")
-    FVector2D SpawnAreaMax = FVector2D(500.f, 500.f);
+  /** Maximum XY (in local space) for random spawn positions. */
+  UPROPERTY(EditAnywhere, Category = "WorldMap")
+  FVector2D SpawnAreaMax = FVector2D(500.f, 500.f);
 
-    /** Maximum distance to consider two territories adjacent. */
-    UPROPERTY(EditAnywhere, Category = "WorldMap")
-    float AdjacencyDistance = 210.f;
+  /** Maximum distance to consider two territories adjacent. */
+  UPROPERTY(EditAnywhere, Category = "WorldMap")
+  float AdjacencyDistance = 210.f;
 
 protected:
-    /** Predefined territory table used for spawning at begin play. */
-    static const TArray<FTerritorySpawnData> DefaultTerritories;
+  /** Predefined territory table used for spawning at begin play. */
+  static const TArray<FTerritorySpawnData> DefaultTerritories;
 };
-
