7d809f04ebd493bfd89fece41a6bb26c47708919
Matrocoles <83141363+Matrocoles@users.noreply.github.com>
2025-10-24 12:39:01 -0400

    Revert "Fix missing WidgetTree include path"

--- Source/Skald/UI/PrepareForBattleWidget.cpp
+++ Source/Skald/UI/PrepareForBattleWidget.cpp
@@ -7,7 +7,7 @@
 #include "Components/VerticalBoxSlot.h"
 #include "Types/SlateEnums.h"
 #include "Types/SlateStructs.h"
-#include "Blueprint/WidgetTree.h"
+#include "WidgetTree.h"
 
 void UPrepareForBattleWidget::NativeOnInitialized() {
   Super::NativeOnInitialized();
