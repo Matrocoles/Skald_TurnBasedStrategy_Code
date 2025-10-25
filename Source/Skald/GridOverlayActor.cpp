b4b0822a9a16668ed89fd32b0608691189729d8a
Matrocoles <83141363+Matrocoles@users.noreply.github.com>
2025-10-25 18:19:50 -0400

    Fix GridOverlayActor shuffle include for UE 5.5

--- Source/Skald/GridOverlayActor.cpp
+++ Source/Skald/GridOverlayActor.cpp
@@ -1,5 +1,5 @@
 #include "GridOverlayActor.h"
-#include "Algo/Shuffle.h"
+#include "Algo/RandomShuffle.h"
 #include "CollisionQueryParams.h"
 #include "Components/SceneComponent.h"
 #include "Engine/EngineTypes.h"
@@ -184,7 +184,7 @@ void AGridOverlayActor::SpawnRandomObstacles() {
     return;
   }
 
-  Algo::Shuffle(PotentialAnchors, RandomStream);
+  Algo::RandomShuffle(PotentialAnchors, RandomStream);
 
   if (PotentialAnchors.Num() < TotalSpawnCount) {
     int32 CellsRemaining = PotentialAnchors.Num();
