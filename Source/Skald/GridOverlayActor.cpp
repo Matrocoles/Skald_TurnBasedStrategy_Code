8e8cc0d1f3ad7c118c36a05b2e81b5fbd89200da
Matrocoles <83141363+Matrocoles@users.noreply.github.com>
2025-10-25 17:55:03 -0400

    Fix RandomShuffle usage for obstacle anchor selection

--- Source/Skald/GridOverlayActor.cpp
+++ Source/Skald/GridOverlayActor.cpp
@@ -184,7 +184,7 @@ void AGridOverlayActor::SpawnRandomObstacles() {
     return;
   }
 
-  Algo::RandomShuffle(PotentialAnchors, RandomStream);
+  RandomStream.Shuffle(PotentialAnchors);
 
   if (PotentialAnchors.Num() < TotalSpawnCount) {
     int32 CellsRemaining = PotentialAnchors.Num();
