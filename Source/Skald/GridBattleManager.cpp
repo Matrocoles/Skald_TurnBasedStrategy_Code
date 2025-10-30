af65153f0d51d9386151682a29573522a1fca601
Matrocoles <83141363+Matrocoles@users.noreply.github.com>
2025-10-30 02:09:02 -0400

    Ensure attack FX multicast from battle manager

--- Source/Skald/GridBattleManager.cpp
+++ Source/Skald/GridBattleManager.cpp
@@ -707,6 +707,11 @@ void UGridBattleManager::ReportAttackResolution(AFighterPawn* Attacker, AFighter
         }
     }
 
+    if (Attacker)
+    {
+        Attacker->TriggerAttackPresentationFX(Defender);
+    }
+
     const bool bHadListeners = OnAttackResolved.IsBound();
     OnAttackResolved.Broadcast(Attacker, Defender, Result);
 
--- Source/Skald/Skald_PlayerController.cpp
+++ Source/Skald/Skald_PlayerController.cpp
@@ -5126,9 +5126,6 @@ void ASkaldPlayerController::PlayAttackFeedback(
   FDiceRollOutcome SyntheticOutcome;
   SyntheticOutcome.bHit = Result.TotalDamage > 0;
   SyntheticOutcome.bCritical = Result.CriticalHitCount > 0;
-  if (Attacker) {
-    Attacker->TriggerAttackPresentationFX(Defender);
-  }
   PlayDiceOutcomeFeedback(Attacker, Defender, SyntheticOutcome);
 }
 
@@ -5487,9 +5484,6 @@ void ASkaldPlayerController::HandleDiceResolutionComplete(
 void ASkaldPlayerController::HandleDiceOutcomeRevealed(
     AFighterPawn *Attacker, AFighterPawn *Defender,
     const FDiceRollOutcome &Outcome, int32 RevealIndex) {
-  if (Attacker) {
-    Attacker->TriggerAttackPresentationFX(Defender);
-  }
   PlayDiceOutcomeFeedback(Attacker, Defender, Outcome);
 
   if (Defender && Outcome.bHit) {
